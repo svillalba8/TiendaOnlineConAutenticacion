b5d230f6e5af54f5f384ffd13626f3ee807507a8
Sergio <svillalba5@educa.madrid.org>
2025-02-03 20:12:40 +0100

    Commit inicial

---
+++ src/main/java/gamo/villalba/sergio/SergioApplication.java
@@ -0,0 +1,13 @@
+package gamo.villalba.sergio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gamo/villalba/sergio/SergioApplicationTests.java
@@ -0,0 +1,13 @@
+package gamo.villalba.sergio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
